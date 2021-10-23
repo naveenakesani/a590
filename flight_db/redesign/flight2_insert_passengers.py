eb393a01f97006a4c999941afeeec96604d6e43f
Jade-flow <82106103+Jade-flow@users.noreply.github.com>
2021-10-23 18:47:11 -0500

    flight2 insert passengers by pandas.to_sql

--- flight_db/redesign/flight2_insert_passengers.py
+++ flight_db/redesign/flight2_insert_passengers.py
@@ -26,7 +26,7 @@ db = config.sqlalchemy_engine("flight2")
 # transaction
 if db is not None:
     #insert, three modes:fail,replace,append. index to False, skip row index
-    df.to_sql('passenger', con=db, if_exists='replace', index=False) 
+    df.to_sql('passenger', con=db, if_exists='append', index=False) 
     #airlines_df.to_csv(f, index=False, line_terminator='\n')
     test = pd.read_sql("select * from passenger", db)
     print(test[0:5])
