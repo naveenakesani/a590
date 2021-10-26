308fa3813c0489e516fae5964fd9fb2ffb1da038
Jade-flow <82106103+Jade-flow@users.noreply.github.com>
2021-10-26 16:26:22 -0500

    configuration, connect functions

--- flight_db/function/config.py
+++ flight_db/function/config.py
@@ -4,15 +4,15 @@ place config.py in the subdirectory `function`
 # mysql
 mysql_host = "127.0.0.1"
 mysql_user = "root"
-mysql_pwd = xxxx
+mysql_pwd = "apple"
 #mysql_pwd_mac = ""
-mysql_port = xxxx
-#mysql_port_mac=xxxx
+mysql_port = 3306
+#mysql_port_mac = 3306
 
 import mysql.connector as mc
 from mysql.connector import errorcode
 from sqlalchemy import create_engine, types
-import pymysql
+import pymysql, sqlite3
 
 def connect_mysql(db_name):
     try:
@@ -30,13 +30,18 @@ def connect_mysql(db_name):
         print(f"Successful connection to {db_name}")
         return db
 
-def sqlalchemy_engine(db_name):
+def sqlalchemy_sqlite3(db_name):
+    engine = create_engine(f"sqlite:///{db_name}")
+    return engine
+
+def sqlalchemy_mysql(db_name):
     try:
-        engine = create_engine(f'mysql+pymysql://{mysql_user}:{mysql_pwd}@127.0.0.1:{mysql_port}/{db_name}?charset=utf8mb4')
-        mysql_engine = engine.connect()
+        db_uri = f'mysql+pymysql://{mysql_user}:{mysql_pwd}@127.0.0.1:{mysql_port}/{db_name}?charset=utf8mb4'
+        engine = create_engine(db_uri)
+        #engine = engine.connect()
     except pymysql.Error as err:
         print(f'Connect failed: {err}')
         return None
     else:
         print(f"Successful connection to MySQL {db_name}")
-        return mysql_engine
+        return engine
