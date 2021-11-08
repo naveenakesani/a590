a6ae3791e78cc3d52d00ac0bcadfb98ec18c0aab
Jade-flow <82106103+Jade-flow@users.noreply.github.com>
2021-11-08 12:33:59 -0600

    Update config.py

--- flight_db/function/config.py
+++ flight_db/function/config.py
@@ -9,8 +9,6 @@ mysql_pwd = "apple"
 mysql_port = 3306
 #mysql_port_mac = 3306
 
-import mysql.connector as mc
-from mysql.connector import errorcode
 from sqlalchemy import create_engine, types
 import pymysql, sqlite3
 
